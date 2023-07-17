80fe68c9c8face757e279b3e1b9173fa26943024
Pritam Kumar <pritamkumar@Pritams-MacBook-Air.local>
2023-07-17 10:33:03 +0530

    feat: search bar to fetch location

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import { MainContainer } from "../../components";
-
+import { Title } from "../../widgets";
+import * as BiIcons from "react-icons/bi";
 const Home = () => {
-  return <div className={styles.pageContainer}><h1>Home</h1></div>;
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title text={"Air pollution"} />
+        </div>
+        <div className={styles.right}>
+          <div className={styles.searchBar}>
+            <input type="text" placeholder="Search" className={styles.input}/>{" "}
+            <button className={styles.btnSearch}>
+              <BiIcons.BiSearch />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
